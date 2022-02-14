eae4e357d02d97b43251697816c382295820ce75
renjith s raj <renjithsraj@daemon.local>
2022-02-14 22:20:37 +0530

    Updated: Added Permission Model

--- blackmonk/apps/users/models.py
+++ blackmonk/apps/users/models.py
@@ -4,6 +4,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from ckeditor.fields import RichTextField
 from django.utils.translation import gettext as _
+from django.contrib.contenttypes.models import ContentType
+
+class BaseModel(models.Model):
+    """ This is will not create an table in database """
+    created = models.DateTimeField(_('Created'), auto_now_add=True)
+    updated = models.DateTimeField(_('Updated'), auto_now=True)
+    is_active = models.BooleanField(_('Active'), default=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserPermission(BaseModel):
+    role = models.CharField(_('Role'), max_length=255, null=True, blank=True)
+    permissions = models.CharField(_('Permissions'), null=True, blank=True, max_length=255)
+    model = models.ForeignKey(ContentType, related_name='permissions', on_delete=models.CASCADE)
+    permission_code = models.CharField(_('Code'), max_length=255, null=True, blank=True)
+    permission_name = models.CharField(_('Name'), max_length=255, null=True, blank=True)
+    group = models.CharField(_('Group'), max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.permission_name
+
+
 
 
 
