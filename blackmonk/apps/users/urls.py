bb5c9fdf804e4241aea3cc16e0676ef3477fe217
renjith s raj <renjithsraj@daemon.local>
2022-02-17 23:14:55 +0530

    Adding -> Templates

--- blackmonk/apps/users/urls.py
+++ blackmonk/apps/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from apps.users.views import home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
