558dabce246f822c8b7377fbbee99862fbb1450d
renjith s raj <renjithsraj@daemon.local>
2022-02-18 21:34:42 +0530

    Updated: registration and login html

--- blackmonk/apps/users/urls.py
+++ blackmonk/apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.users.views import home
+from apps.users.views import home, RegisterView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),
-    path('login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/register', RegisterView.as_view(), name='register'),
 ]
--- blackmonk/apps/users/views.py
+++ blackmonk/apps/users/views.py
@@ -1,6 +1,12 @@
+from typing import ValuesView
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+class RegisterView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'registration/registration.html', {})
