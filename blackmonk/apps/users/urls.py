f137e8d30dbfe7d5afc5f759be077417c5ad0690
renjith s raj <renjithsraj@daemon.local>
2022-02-22 06:33:14 +0530

    Updated -> email activation emails

--- blackmonk/apps/users/urls.py
+++ blackmonk/apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.users.views import home, RegisterView
+from apps.users.views import home, RegisterView, ActivateAccount
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),
     path('accounts/login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/register', RegisterView.as_view(), name='register'),
+    path('activate/<uidb64>/<token>/', ActivateAccount.as_view(), name='activate'),
 ]
--- blackmonk/apps/users/views.py
+++ blackmonk/apps/users/views.py
@@ -2,6 +2,7 @@ from typing import ValuesView
 from django.shortcuts import render
 from django.views import View
 from apps.users.forms import UserForm
+from apps.users.models import User
 from django.shortcuts import redirect
 
 from apps.users.tokens import account_activation_token
@@ -10,6 +11,11 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode
+
+from django.contrib.auth import login
+from django.utils.encoding import force_text
+
 # Create your views here.
 
 def home(request):
@@ -22,7 +28,7 @@ class RegisterView(View):
     
     def prepare_for_activate_mail(self, *args, **kwargs):
         try:
-            current_site = kwargs.get('request')
+            current_site = get_current_site(kwargs.get('request'))
             user = kwargs.get('user')
             subject = "Activate your BlackMonk Account"
             message = render_to_string('emails/account_activation_email.html', {
@@ -48,4 +54,23 @@ class RegisterView(View):
             self.prepare_for_activate_mail(request=request, user=user)
             return redirect('/accounts/login')
         return render(request, 'registration/registration.html', {'form': form})
+
+class ActivateAccount(View):
     
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.profile.email_confirmed = True
+            user.save()
+            login(request, user)
+            messages.success(request, ('Your account have been confirmed.'))
+            return redirect('home')
+        else:
+            messages.warning(request, ('The confirmation link was invalid, possibly because it has already been used.'))
+            return redirect('home')
